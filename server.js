70102bde1d2e60b8ee8e7ec95fd172b20e9f40e7
Meow Roar <sareacct91@gmail.com>
2024-01-18 21:14:15 -0800

    commit

--- server.js
+++ server.js
@@ -27,6 +27,8 @@ app.get('/feedback', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
 );
 
+app.use('*', (req,res) => sendFile)
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
